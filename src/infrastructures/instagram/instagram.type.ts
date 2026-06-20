952405b00c247a47f139ae105507e2ecef58d845
sudosubin <sudosubin@gmail.com>
2026-06-20 22:06:30 +0900

    chore: Update instgram post type properties

--- src/infrastructures/instagram/instagram.type.ts
+++ src/infrastructures/instagram/instagram.type.ts
@@ -1,15 +1,17 @@
+export interface InstagramAddress {
+  streetAddress: string;
+  zipCode: string;
+  cityName: string;
+  regionName: string;
+  countryCode: string;
+}
+
 export interface InstagramLocation {
   id: string;
   name: string;
   slug: string;
   hasPublicPage: boolean;
-  address: {
-    street_address: string;
-    zip_code: string;
-    city_name: string;
-    region_name: string;
-    country_code: string;
-  } | null;
+  address: InstagramAddress | null;
 }
 
 export interface InstagramOwner {
@@ -19,10 +21,10 @@ export interface InstagramOwner {
 }
 
 export interface ScrapedPost {
-  owner: InstagramOwner;
-  shortcode: string;
-  typename: "image" | "video" | "carousel"; // XDTGraphImage, XDTGraphVideo, XDTGraphSidecar
-  caption: string | null;
-  imageUrls: string[];
-  location: InstagramLocation | null;
+  owner: InstagramOwner; // 계정명 등 정보
+  shortcode: string; // 게시글 식별자
+  typename: "image" | "video" | "carousel";
+  caption: string | null; // 게시글 본문
+  imageUrls: string[]; // 사진 + 영상 썸네일
+  location: InstagramLocation | null; // 위치 정보
 }
