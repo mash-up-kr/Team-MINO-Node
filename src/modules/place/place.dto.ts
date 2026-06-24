63e49d42b5aa76e91a2706febb3233dd69b09240
minsour <sparkm0128@gmail.com>
2026-06-25 05:39:21 +0900

    chore: Kakao geocoder 확인용 API 추가

--- src/modules/place/place.dto.ts
+++ src/modules/place/place.dto.ts
@@ -8,3 +8,13 @@ export const createPlaceRequestSchema = v.object({
 });
 
 export type CreatePlaceRequest = v.InferOutput<typeof createPlaceRequestSchema>;
+
+// TODO: KakaoProvider 확인용 임시 의존성으로 전체 플로우 연동 후 제거합니다.
+export const testGeocodeRequestSchema = v.object({
+  placeName: v.pipe(v.string(), v.minLength(1)),
+  areaName: v.pipe(v.string(), v.minLength(1)),
+  areaType: v.optional(v.picklist(["landmark", "address", "region"])),
+});
+
+// TODO: KakaoProvider 확인용 임시 의존성으로 전체 플로우 연동 후 제거합니다.
+export type TestGeocodeRequest = v.InferOutput<typeof testGeocodeRequestSchema>;
