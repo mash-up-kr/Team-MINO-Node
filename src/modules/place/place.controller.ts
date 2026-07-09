765148eb92cb79c0a955f58403d66802f76d7fec
minsour <sparkm0128@gmail.com>
2026-07-10 02:35:34 +0900

    chore: 전체 플로우 연동 완료로 _test/geocode 임시 엔드포인트 제거
    임시 지오코딩 확인용 엔드포인트와 관련 DTO(testGeocodeRequestSchema)를
    제거하고, 컨트롤러의 GeocoderService 직접 의존도 정리한다.

--- src/modules/place/place.controller.ts
+++ src/modules/place/place.controller.ts
@@ -1,23 +1,12 @@
-import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { ValibotPipe } from "../../common/pipes/valibot.pipe";
-import { GeocoderService } from "../../infrastructures/geocoder/geocoder.service";
-import type { GeoCandidate } from "../../infrastructures/geocoder/geocoder.type";
-import {
-  type CreatePlaceRequest,
-  createPlaceRequestSchema,
-  type TestGeocodeRequest,
-  testGeocodeRequestSchema,
-} from "./place.dto";
+import { type CreatePlaceRequest, createPlaceRequestSchema } from "./place.dto";
 import { PlaceService } from "./place.service";
 import type { PlaceMatch } from "./place.type";
 
 @Controller("api/v1/place")
 export class PlaceController {
-  constructor(
-    private readonly placeService: PlaceService,
-    // TODO: KakaoProvider 확인용 임시 의존성으로 전체 플로우 연동 후 제거합니다.
-    private readonly geocoderService: GeocoderService,
-  ) {}
+  constructor(private readonly placeService: PlaceService) {}
 
   @Post("places")
   async createPlace(
@@ -29,17 +18,5 @@ export class PlaceController {
       default:
         throw new Error(`Unsupported method: ${body.method}`);
     }
-  }
-
-  // TODO: KakaoProvider 확인용 임시 테스트 엔드포인트로 전체 플로우 연동 후 제거합니다.
-  @Get("_test/geocode")
-  async testGeocode(
-    @Query(new ValibotPipe(testGeocodeRequestSchema)) query: TestGeocodeRequest,
-  ): Promise<GeoCandidate[]> {
-    return this.geocoderService.searchAll({
-      placeName: query.placeName,
-      areaName: query.areaName,
-      areaType: query.areaType,
-    });
   }
 }
--- src/modules/place/place.dto.ts
+++ src/modules/place/place.dto.ts
@@ -1,5 +1,4 @@
 import * as v from "valibot";
-import { AREA_TYPES } from "../../infrastructures/geocoder/geocoder.type";
 
 export const createPlaceRequestSchema = v.object({
   method: v.picklist(["instagram_url"]),
@@ -9,13 +8,3 @@ export const createPlaceRequestSchema = v.object({
 });
 
 export type CreatePlaceRequest = v.InferOutput<typeof createPlaceRequestSchema>;
-
-// TODO: KakaoProvider 확인용 임시 의존성으로 전체 플로우 연동 후 제거합니다.
-export const testGeocodeRequestSchema = v.object({
-  placeName: v.pipe(v.string(), v.minLength(1)),
-  areaName: v.pipe(v.string(), v.minLength(1)),
-  areaType: v.optional(v.picklist(AREA_TYPES)),
-});
-
-// TODO: KakaoProvider 확인용 임시 의존성으로 전체 플로우 연동 후 제거합니다.
-export type TestGeocodeRequest = v.InferOutput<typeof testGeocodeRequestSchema>;
