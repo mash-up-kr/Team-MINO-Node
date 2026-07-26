fd5e3feac73dec564eac26bf20c5e7cd08ff4265
Park Minsoo <parkminsoo0128@gmail.com>
2026-07-26 23:37:42 +0900

    feat: 클라 개발용 임시 place mock 엔드포인트 추가 (#41)
    feat: 클라 개발용 임시 mock 엔드포인트 추가

--- src/modules/place/place.module.ts
+++ src/modules/place/place.module.ts
@@ -3,11 +3,13 @@ import { AiModule } from "../../infrastructures/ai/ai.module";
 import { GeocoderModule } from "../../infrastructures/geocoder/geocoder.module";
 import { ScraperModule } from "../../infrastructures/scraper/scraper.module";
 import { PlaceController } from "./place.controller";
+// TODO(임시): 클라 개발 언블록용 mock. 실제 파이프라인 연동되면 제거.
+import { PlaceMockController } from "./place.mock.controller";
 import { PlaceService } from "./place.service";
 
 @Module({
   imports: [AiModule, GeocoderModule, ScraperModule],
-  controllers: [PlaceController],
+  controllers: [PlaceController, PlaceMockController],
   providers: [PlaceService],
 })
 export class PlaceModule {}
