cf81f85eb0e9f6f1084604ebba916050e662e34a
kkardy <wlstjd4013@gmail.com>
2026-08-16 20:01:24 +0900

    refactor: 핀 컬럼 상수를 pin.repository 모듈 상수로 이동
    Drizzle 스키마에 의존하는 select 컬럼 집합은 사용처인 repository의
    모듈 레벨 상수로 두기로 리뷰(#55) 방향 반영. pin.constant.ts 삭제.

--- src/modules/pin/pin.repository.ts
+++ src/modules/pin/pin.repository.ts
@@ -6,7 +6,6 @@ import { rooms } from "../room/room.schema";
 import { roomMembers } from "../room/room-member.schema";
 import { sources } from "../source/source.schema";
 import { users } from "../user/user.schema";
-import { PIN_AUTHOR_COLUMNS, PIN_COLUMNS } from "./pin.constant";
 import { pins } from "./pin.schema";
 import type {
   PinForUserRow,
@@ -15,6 +14,23 @@ import type {
   TargetRoomRow,
 } from "./pin.type";
 import { pinAccesses } from "./pin-access.schema";
+
+/**
+ * 핀 응답에 노출하는 컬럼 집합. drizzle은 entity 클래스 없이 테이블
+ * 정의가 곧 스키마라, 내부 컬럼이 응답에 새지 않도록 select 대상을 상수로 고정한다.
+ */
+const PIN_COLUMNS = {
+  id: pins.id,
+  roomId: pins.roomId,
+  createdAt: pins.createdAt,
+};
+
+/** 핀을 저장한 멤버("누가 추가한 곳") 표시용 프로필 컬럼 집합. */
+const PIN_AUTHOR_COLUMNS = {
+  userId: users.id,
+  nickname: users.nickname,
+  avatar: users.avatar,
+};
 
 @Injectable()
 export class PinRepository extends BaseRepository {
